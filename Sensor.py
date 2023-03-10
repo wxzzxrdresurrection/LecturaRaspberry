0959adbddcb8c021932c699f0c1eba22b32f0b12
Luis Angel Zapata <magicwizz12@gmail.com>
2023-03-10 12:16:48 -0600

    PRUEBA RPI

--- Sensor.py
+++ Sensor.py
@@ -115,8 +115,8 @@ class Sensor(Lista):
                 print("Esperando")
                 pulse_start = time.time()
             while GPIO.input(echo) == GPIO.HIGH:
-                print("Esperando2")
                 pulse_end = time.time()
+                GPIO.input(echo, GPIO.LOW) 
             pulse_duration = pulse_end - pulse_start
             distance = pulse_duration * 17150
             distance = round(distance, 2)
