7324cfbfadfcc3b63f69f4c5060261d6b0e0b4d5
wxzzxrdresurrection <magicwizz12@gmail.com>
2023-03-23 23:27:07 -0600

    YA NO JALA TEMPERATURA

--- Menu.py
+++ Menu.py
@@ -23,7 +23,6 @@ class Menu:
             elif opcion == "2":
                 self.sns.read(temp)
             elif opcion == "3":
-                print("Led")
                 self.sns.read(led)
             elif opcion == "4":
                 self.sns.readTodos()
